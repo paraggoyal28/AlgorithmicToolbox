5ac8fae5aee3b019a6f48f9cbb02ef8b6b9805f9
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2023-12-05 22:18:41 +0530

    add first two programs for week2 and added the problem statement sheet

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.cpp
@@ -23,8 +23,18 @@ int fibonacci_naive(int n) {
 
 int fibonacci_fast(int n) {
     // write your code here
-
-    return 0;
+	if(n <= 1) {
+		return n;
+	}
+	int a = 0;
+	int b = 1;
+	for(int i = 2; i <= n; ++i) {
+		int c = a + b;
+		a = b;
+		b = c;
+	}
+	
+    return b;
 }
 
 void test_solution() {
@@ -38,8 +48,9 @@ int main() {
     int n = 0;
     std::cin >> n;
 
-    std::cout << fibonacci_naive(n) << '\n';
+//    std::cout << fibonacci_naive(n) << '\n';
+    std::cout << fibonacci_fast(n) << '\n';
     //test_solution();
-    //std::cout << fibonacci_fast(n) << '\n';
+
     return 0;
 }
--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstring>
+#define ll long long int
+using std::cout;
 
 int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
@@ -10,15 +13,38 @@ int get_fibonacci_last_digit_naive(int n) {
     for (int i = 0; i < n - 1; ++i) {
         int tmp_previous = previous;
         previous = current;
-        current = tmp_previous + current;
+        current = (tmp_previous + current)%10;
     }
 
     return current % 10;
 }
 
+ll fib(ll f[], ll n)
+{
+    // 0th and 1st number of
+    // the series are 0 and 1
+    f[0] = 0;
+    f[1] = 1;
+ 
+    // Add the previous 2 numbers 
+    // in the series and store 
+    // last digit of result
+    for (ll i = 2; i <= n; i++)
+        f[i] = (f[i - 1] + f[i - 2]) % 10;
+ 
+    return f[n];
+}
+
+int get_fibonacci_last_digit_fast(int n) {
+	ll f[60] = {0};
+	fib(f, 60);
+	return f[n%60];
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+ //   int c = get_fibonacci_last_digit_naive(n);
+  	int c = get_fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
-    }
+}
