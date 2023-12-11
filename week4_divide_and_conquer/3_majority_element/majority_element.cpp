c304169147069754722b08f241448fa28395bff3
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2023-12-11 22:43:02 +0530

    add code for week4 for problems 1 to 5

--- week4_divide_and_conquer/3_majority_element/majority_element.cpp
+++ week4_divide_and_conquer/3_majority_element/majority_element.cpp
@@ -4,18 +4,51 @@
 
 using std::vector;
 
-int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+int get_majority_element(vector<int> &a, int left, int right)
+{
+  int n = a.size();
+  if (left == right)
+    return -1;
+  if (left + 1 == right)
+    return a[left];
+  int count = 1;
+  int element = a[left];
+  left += 1;
+  while (left < right)
+  {
+    if (a[left] == element)
+    {
+      count += 1;
+    }
+    else
+    {
+      count -= 1;
+    }
+    if (count == 0)
+    {
+      element = a[left];
+      count = 1;
+    }
+    left += 1;
+  }
+  count = 0;
+  for (int i = 0; i < n; ++i)
+  {
+    if (element == a[i])
+    {
+      count += 1;
+    }
+  }
+  return count > n / 2 ? element : -1;
 }
 
-int main() {
+int main()
+{
   int n;
   std::cin >> n;
   vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
+  for (size_t i = 0; i < a.size(); ++i)
+  {
     std::cin >> a[i];
   }
   std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
--- week4_divide_and_conquer/5_number_of_inversions/inversions.cpp
+++ week4_divide_and_conquer/5_number_of_inversions/inversions.cpp
@@ -3,23 +3,61 @@
 
 using std::vector;
 
-long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
+long long merge(vector<int> &a, vector<int> &b, size_t left, size_t ave, size_t right)
+{
+  int l = left;
+  int r = ave + 1;
+  long long inversions = 0;
+  int k = left;
+  while (l <= ave && r <= right)
+  {
+    if (a[l] <= a[r])
+    {
+      b[k++] = a[l++];
+    }
+    else
+    {
+      inversions += ave - l + 1;
+      b[k++] = a[r++];
+    }
+  }
+  while (l <= ave)
+  {
+    b[k++] = a[l++];
+  }
+  while (r <= right)
+  {
+    b[k++] = a[r++];
+  }
+  for (int i = left; i <= right; ++i)
+  {
+    a[i] = b[i];
+  }
+  return inversions;
+}
+
+long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right)
+{
   long long number_of_inversions = 0;
-  if (right <= left + 1) return number_of_inversions;
+  if (right <= left)
+    return number_of_inversions;
   size_t ave = left + (right - left) / 2;
   number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
+  number_of_inversions += get_number_of_inversions(a, b, ave + 1, right);
+  // write your code here
+  number_of_inversions += merge(a, b, left, ave, right);
   return number_of_inversions;
 }
 
-int main() {
+int main()
+{
   int n;
   std::cin >> n;
   vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
+  for (size_t i = 0; i < a.size(); i++)
+  {
     std::cin >> a[i];
   }
   vector<int> b(a.size());
-  std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+  std::cout << get_number_of_inversions(a, b, 0, a.size() - 1) << '\n';
 }
